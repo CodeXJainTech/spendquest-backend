ec4e00829207dc9cefa31440ada4c9e2e5c9496b
Anchal Jain <anchaljain1106@gmail.com>
2025-08-14 21:18:54 +0530

    testing backend

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,7 @@ const mainRouter = require("./routes/main.js");
 const port = process.env.PORT || 3000;
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://spendquest.vercel.app"
-];
-
-app.use(cors({ origin: allowedOrigins }));
+app.use(cors({ origin: "https://spendquest.vercel.app" }));
 app.use(express.json());
 app.use("/api/v1", mainRouter);
 
