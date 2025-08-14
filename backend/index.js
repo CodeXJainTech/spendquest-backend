a5d17b8c77bc8f742db2299d1cc7a5f4984fcf14
Anchal Jain <anchaljain1106@gmail.com>
2025-08-14 18:45:05 +0530

    backend port added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mainRouter = require("./routes/main.js");
 const cors = require('cors')
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
